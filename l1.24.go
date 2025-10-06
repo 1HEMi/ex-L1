cc74e55e3429956e418135418a02655e8c161fa5
Ruslan Knyazev <knyazev12122003@mail.ru>
2025-10-06 17:37:30 +0300

    added ex l1.24

---
+++ l1.24.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	x float64
+	y float64
+}
+
+func NewPoint(x, y float64) Point {
+	return Point{x: x, y: y}
+}
+
+func (p Point) Distance(other Point) float64 {
+	dx := p.x - other.x
+	dy := p.y - other.y
+	return math.Sqrt(dx*dx + dy*dy)
+}
+
+func main() {
+	p1 := NewPoint(2, 3)
+	p2 := NewPoint(5, 6)
+	fmt.Printf("Расстояние между точками: %.2f\n", p1.Distance(p2))
+}
